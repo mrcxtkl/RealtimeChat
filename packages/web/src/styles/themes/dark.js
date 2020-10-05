0af5e6e7a7db9297c7ad08dc36d553765f7fd8ff
Marco <mrcxtkl@gmail.com>
2020-10-05 14:28:21 -0300

    Create dark and light theme

---
+++ packages/web/src/styles/themes/dark.js
@@ -0,0 +1,14 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#333',
+    secondary: '#DB3069',
+    tertiary: '#F5D547',
+    toggle: '#DB3069', // '#F5D547'
+
+    background: '#222',
+    title: '#EBEBD3',
+    text: '#EBEBD3'
+  }
+}
---
+++ packages/web/src/styles/themes/light.js
@@ -0,0 +1,14 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#DB3069',
+    secondary: '#1446A0',
+    tertiary: '#F5D547',
+    toggle: '#1446A0', // '#F5D547'
+
+    background: '#EBEBD3',
+    title: '#EBEBD3',
+    text: '#3C3C3B'
+  }
+}
