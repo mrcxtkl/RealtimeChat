36c8de506b2d0e8acb37e01f284105e557e8f682
Marco <mrcxtkl@gmail.com>
2020-10-04 23:08:54 -0300

    Add SocketProvider to application

--- packages/web/src/App.jsx
+++ packages/web/src/App.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
 
-const App = () => <h1>aoba</h1>
+import { SocketProvider, useSocket } from './contexts/Socket'
+
+import Chat from './pages/Chat'
+import Register from './pages/Register'
+
+const App = () => {
+  const { hasSocket } = useSocket()
+
+  return (
+    <SocketProvider>
+      { hasSocket ? <Chat /> : <Register /> }
+    </SocketProvider>
+  )
+}
 
 export default App
