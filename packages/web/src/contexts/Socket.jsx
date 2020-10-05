82b22e17dc4cf234837b365cea7e371ef2f72b69
Marco <mrcxtkl@gmail.com>
2020-10-05 14:25:52 -0300

    Add self connect to Socket Provider

--- packages/web/src/contexts/Socket.jsx
+++ packages/web/src/contexts/Socket.jsx
@@ -1,22 +1,26 @@
-import React, { useState, createContext, useContext } from 'react'
+import React, { useState, createContext, useContext, useEffect } from 'react'
 import io from 'socket.io-client'
 
-const SocketContext = createContext({ socket: null })
+const SocketContext = createContext({})
 
 export const SocketProvider = ({ children }) => {
-  const [ socket, setSocket ] = useState(null)
+  const [socket, setSocket] = useState(null)
+  const [username, setUsername] = useState()
 
   async function connect() {
     const newWS = io(process.env.SOCKET_HOST || 'localhost:3333')
     setSocket(newWS)
   }
 
+  useEffect(() => {
+    connect()
+  }, [])
+
   return (
-    <SocketContext.Provider value={{ hasSocket: !!socket, connect, socket}}>
+    <SocketContext.Provider value={{ hasSocket: !!socket, connect, socket, setUsername, username }}>
       {children}
     </SocketContext.Provider>
   )
 }
-export default SocketContext
 
 export const useSocket = () => useContext(SocketContext)
